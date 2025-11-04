0dff9b2458b2f7944237a5ad06007128bd1e6848
fadymilad650-creator <fadymilad650@gmail.com>
2025-11-04 02:26:15 +0200

    Add files via upload

---
+++ icons.tsx
@@ -0,0 +1,85 @@
+
+import React from 'react';
+
+export const ChurchIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M18 7.5a6 6 0 0 0-12 0" />
+    <path d="M14 22v-4a2 2 0 0 0-2-2 2 2 0 0 0-2 2v4" />
+    <path d="M6 22V9" />
+    <path d="M18 22V9" />
+    <path d="M12 2v5" />
+    <path d="M10 2h4" />
+    <path d="m14.5 9.5-2.5 2.5-2.5-2.5" />
+    <path d="m14.5 13.5-2.5 2.5-2.5-2.5" />
+  </svg>
+);
+
+export const BathroomIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M9 6a3 3 0 0 1 3-3v0a3 3 0 0 1 3 3v3" />
+    <path d="M12 12h.01" />
+    <path d="M15 9H9" />
+    <path d="M18 12a6 6 0 0 1-6 6v0a6 6 0 0 1-6-6v-1a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v1Z" />
+    <path d="M6 18v2a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-2" />
+  </svg>
+);
+
+export const CafeteriaIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M17 8h-7a3 3 0 0 0-3 3v5a3 3 0 0 0 3 3h7a3 3 0 0 0 3-3v-5a3 3 0 0 0-3-3Z" />
+    <path d="M4.22 12.22a3 3 0 0 0 0 4.24l1.06 1.06a3 3 0 0 0 4.24 0" />
+    <path d="M18 8V5a2 2 0 0 0-2-2h-1" />
+  </svg>
+);
+
+export const FishingIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="m15 11-3.5 3.5" />
+    <path d="m20.5 5.5-3 3" />
+    <path d="m2 2 20 20" />
+    <path d="M11.56 16.52a6.47 6.47 0 0 0-1.07-8.03A6.47 6.47 0 0 0 2.46 7.4a6.47 6.47 0 0 0 8.04 1.06" />
+  </svg>
+);
+
+export const QRIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <rect width="5" height="5" x="3" y="3" rx="1" />
+    <rect width="5" height="5" x="16" y="3" rx="1" />
+    <rect width="5" height="5" x="3" y="16" rx="1" />
+    <path d="M21 16h-3a2 2 0 0 0-2 2v3" />
+    <path d="M21 21v.01" />
+    <path d="M12 7v3a2 2 0 0 1-2 2H7" />
+    <path d="M3 12h.01" />
+    <path d="M12 3h.01" />
+    <path d="M12 16v.01" />
+    <path d="M16 12h1" />
+    <path d="M21 12v.01" />
+    <path d="M12 21v-1" />
+  </svg>
+);
+
+export const LibraryIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
+    <path d="M8 7h6" />
+    <path d="M8 11h8" />
+  </svg>
+);
+
+export const GardenIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M12 20.5a2.5 2.5 0 0 1-5 0" />
+    <path d="M12 20.5a2.5 2.5 0 0 0 5 0" />
+    <path d="M19 12a7 7 0 1 0-14 0" />
+    <path d="M12 12a2 2 0 0 0-2 2" />
+    <path d="M12 5a2 2 0 0 1 2 2" />
+    <path d="M5 12a2 2 0 0 0 2-2" />
+    <path d="M19 12a2 2 0 0 0-2-2" />
+  </svg>
+);
+
+export const GemIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M6 3h12l4 6-10 13L2 9Z"/><path d="M12 22 6 9l-4-6h12l4 6"/><path d="M2 9h20"/>
+    </svg>
+);
